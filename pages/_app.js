93caf510153860c022bdbeb7b894d47f2f092a62
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-03 19:27:52 -0500

    added boilerplate for mui,emotion

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,37 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../config/theme";
+import createEmotionCache from "../config/createEmotionCache";
+import Navbar from "../components/Navbar";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App({
+    Component,
+    pageProps,
+    emotionCache = clientSideEmotionCache,
+}) {
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>Quizie</title>
+                <meta name="theme-color" content={theme.palette.primary.main} />
+                <meta name="description" content="Quiz maker and taker app." />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Navbar />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
