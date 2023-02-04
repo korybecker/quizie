c3cb92935a7912ee3b25ff1a817d238239b8bb53
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-03 19:29:15 -0500

    removed quizies from home page, just a simple home page now

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,11 @@
-import Head from 'next/head'
-import prisma from '../lib/prisma';
-import QuizBlock from '@/components/QuizBlock';
+import styles from "../styles/Home.module.css";
 
-export const getStaticProps = async () => {
-  const posts = await prisma.quiz.findMany({
-    where: { published: false },
-    include: {
-      creator: {
-        select: { name: true },
-      },
-    },
-  })
-  const allPosts = JSON.stringify(posts)
-  return {
-    props: { allPosts },
-    revalidate: 10
-  }
-}
-
-export default function Home({allPosts}) {
-  allPosts = JSON.parse(allPosts)
-  return (
-    <>
-      <Head>
-        <title>Quizie</title>
-        <meta name="description" content="Quiz maker and taker app." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Quizies</h1>
-      <div>
-        {allPosts.map((post, i) => {
-          return (
-            <QuizBlock key={i} post={post} />
-          )
-        })}
-      </div>
-    </>
-  )
+export default function Home() {
+    return (
+        <>
+            <div className={styles.container}>
+                <h1>Welcome to Quizie</h1>
+            </div>
+        </>
+    );
 }
