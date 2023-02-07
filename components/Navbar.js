24e21d53b575112b052dfccb773a2c49586aee5f
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-07 15:14:22 -0500

    links in hamburger menu now functional, collapsed logo now links to home page

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,15 +13,40 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-
-const loggedInPages = ["Create"];
-const loggedOutPages = ["Quizies", "Create"];
-const loggedInSettings = ["Profile", "Logout"];
-const loggedOutSettings = ["Login", "Signup"];
-const pages = false ? loggedInPages : loggedOutPages;
-const settings = false ? loggedInSettings : loggedOutSettings;
+import { useRouter } from "next/router";
+import { signOut, useSession } from "next-auth/react";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+const theme = createTheme({
+    status: {
+        danger: "#e53e3e",
+    },
+    palette: {
+        primary: {
+            main: "#0971f1",
+            darker: "#053e85",
+        },
+        neutral: {
+            main: "#636363",
+            contrastText: "#fff",
+        },
+    },
+});
 
 function ResponsiveAppBar() {
+    const router = useRouter();
+    const { data: session, status } = useSession();
+
+    const isActive = (pathname) => router.pathname === pathname;
+
+    const pages = session ? ["Quizies", "Create"] : ["Quizies"];
+    const settings = session ? ["Profile", "Logout"] : ["Login"];
+
+    const settingUrls = {
+        Login: "/api/auth/signin",
+        Profile: `/u/${session?.userId}`,
+    };
+
     const [anchorElNav, setAnchorElNav] = useState(null);
     const [anchorElUser, setAnchorElUser] = useState(null);
 
@@ -36,174 +61,210 @@ function ResponsiveAppBar() {
         setAnchorElNav(null);
     };
 
-    const handleCloseUserMenu = () => {
+    const handleCloseUserMenu = (setting) => {
+        console.log(setting);
+        if (setting === "Logout") {
+            signOut();
+            router.push("/");
+        }
         setAnchorElUser(null);
     };
 
     return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <AdbIcon
-                        sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
-                    />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: "none", md: "flex" },
-                            fontFamily: "monospace",
-                            fontWeight: 700,
-                            letterSpacing: ".3rem",
-                            color: "inherit",
-                            textDecoration: "none",
-                        }}
-                    >
-                        quizie
-                    </Typography>
-
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "flex", md: "none" },
-                        }}
-                    >
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "left",
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: "block", md: "none" },
-                            }}
-                        >
-                            {pages.map((page) => (
-                                <Button onClick={handleCloseNavMenu} key={page}>
-                                    <Typography textAlign="center">
-                                        {page}
-                                    </Typography>
-                                </Button>
-                            ))}
-                        </Menu>
-                    </Box>
-                    <AdbIcon
-                        sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
-                    />
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: { xs: "flex", md: "none" },
-                            flexGrow: 1,
-                            fontFamily: "monospace",
-                            fontWeight: 700,
-                            letterSpacing: ".3rem",
-                            color: "inherit",
-                            textDecoration: "none",
-                        }}
-                    >
-                        quizie
-                    </Typography>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "none", md: "flex" },
-                        }}
-                    >
-                        {pages.map((page) => (
-                            <Link
-                                href={`/${page.toLowerCase()}`}
-                                key={page}
-                                passHref
+        <ThemeProvider theme={theme}>
+            <AppBar position="static" color="neutral">
+                <Container maxWidth="xl">
+                    {session && (
+                        <Toolbar disableGutters>
+                            <AdbIcon
+                                sx={{
+                                    display: { xs: "none", md: "flex" },
+                                    mr: 1,
+                                }}
+                            />
+                            <Typography
+                                variant="h6"
+                                noWrap
+                                component="a"
+                                href="/"
+                                sx={{
+                                    mr: 2,
+                                    display: { xs: "none", md: "flex" },
+                                    fontFamily: "monospace",
+                                    fontWeight: 700,
+                                    letterSpacing: ".3rem",
+                                    color: "inherit",
+                                    textDecoration: "none",
+                                }}
                             >
-                                <Button
-                                    key={page}
-                                    onClick={handleCloseNavMenu}
+                                quizie
+                            </Typography>
+
+                            <Box
+                                sx={{
+                                    flexGrow: 1,
+                                    display: { xs: "flex", md: "none" },
+                                }}
+                            >
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleOpenNavMenu}
+                                    color="inherit"
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorElNav}
+                                    anchorOrigin={{
+                                        vertical: "bottom",
+                                        horizontal: "left",
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: "top",
+                                        horizontal: "left",
+                                    }}
+                                    open={Boolean(anchorElNav)}
+                                    onClose={handleCloseNavMenu}
                                     sx={{
-                                        my: 2,
-                                        color: "white",
-                                        display: "block",
+                                        display: { xs: "block", md: "none" },
                                     }}
                                 >
-                                    {page}
-                                </Button>
-                            </Link>
-                        ))}
-                    </Box>
-
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton
-                                onClick={handleOpenUserMenu}
-                                sx={{ p: 0 }}
+                                    {pages.map((page) => (
+                                        <Link
+                                            href={`/${page.toLowerCase()}`}
+                                            key={page}
+                                            passHref
+                                        >
+                                            <Button
+                                                onClick={handleCloseNavMenu}
+                                                key={page}
+                                            >
+                                                <Typography textAlign="center">
+                                                    {page}
+                                                </Typography>
+                                            </Button>
+                                        </Link>
+                                    ))}
+                                </Menu>
+                            </Box>
+                            <AdbIcon
+                                sx={{
+                                    display: { xs: "flex", md: "none" },
+                                    mr: 1,
+                                }}
+                            />
+                            <Typography
+                                variant="h5"
+                                noWrap
+                                component="a"
+                                href="/"
+                                sx={{
+                                    mr: 2,
+                                    display: { xs: "flex", md: "none" },
+                                    flexGrow: 1,
+                                    fontFamily: "monospace",
+                                    fontWeight: 700,
+                                    letterSpacing: ".3rem",
+                                    color: "inherit",
+                                    textDecoration: "none",
+                                }}
                             >
-                                <Avatar
-                                    alt="Remy Sharp"
-                                    src="/public/favicon.ico"
-                                />
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: "45px" }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "right",
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings.map((setting) => (
-                                <MenuItem
-                                    key={setting}
-                                    onClick={handleCloseUserMenu}
-                                >
+                                quizie
+                            </Typography>
+                            <Box
+                                sx={{
+                                    flexGrow: 1,
+                                    display: { xs: "none", md: "flex" },
+                                }}
+                            >
+                                {pages.map((page) => (
                                     <Link
-                                        href={`/${setting.toLowerCase()}`}
+                                        href={`/${page.toLowerCase()}`}
+                                        key={page}
                                         passHref
                                     >
-                                        <Typography textAlign="center">
-                                            {setting}
-                                        </Typography>
+                                        <Button
+                                            key={page}
+                                            onClick={handleCloseNavMenu}
+                                            sx={{
+                                                my: 2,
+                                                color: "white",
+                                                display: "block",
+                                            }}
+                                        >
+                                            {page}
+                                        </Button>
                                     </Link>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
+                                ))}
+                            </Box>
+
+                            <Box sx={{ flexGrow: 0 }}>
+                                <Tooltip title="Open settings">
+                                    <IconButton
+                                        onClick={handleOpenUserMenu}
+                                        sx={{ p: 0 }}
+                                    >
+                                        <Avatar
+                                            alt="Remy Sharp"
+                                            src={session && session.user.image}
+                                        />
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{ mt: "45px" }}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: "top",
+                                        horizontal: "right",
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: "top",
+                                        horizontal: "right",
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    {settings.map((setting) => (
+                                        <MenuItem
+                                            key={setting}
+                                            onClick={() =>
+                                                handleCloseUserMenu(setting)
+                                            }
+                                        >
+                                            {setting in
+                                            {
+                                                Login: "Login",
+                                                Profile: "Profile",
+                                            } ? (
+                                                <Link
+                                                    href={settingUrls[setting]}
+                                                    passHref
+                                                >
+                                                    <Typography textAlign="center">
+                                                        {setting}
+                                                    </Typography>
+                                                </Link>
+                                            ) : (
+                                                <Typography textAlign="center">
+                                                    {setting}
+                                                </Typography>
+                                            )}
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </Box>
+                        </Toolbar>
+                    )}
+                </Container>
+            </AppBar>
+        </ThemeProvider>
     );
 }
 export default ResponsiveAppBar;
