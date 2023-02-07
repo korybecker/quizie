f9ef5cf8903d7e911bc6ca13fe0fa3a7da2132e1
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-07 15:20:11 -0500

    can now get score

--- pages/q/[id].js
+++ pages/q/[id].js
@@ -1,30 +1,92 @@
 import prisma from "@/lib/prisma";
+import QuestionsList from "@/components/QuestionsList";
+import { useState } from "react";
+import styles from "@/styles/Home.module.css";
 
 export const getServerSideProps = async ({ params }) => {
     let quiz = await prisma.quiz.findUnique({
-      where: {
-        id: String(params?.id),
-      },
-      include: {
-        questions: true,
-        creator: true
-      }
+        where: {
+            id: String(params?.id),
+        },
+        include: {
+            questions: {
+                include: {
+                    options: true,
+                },
+            },
+            creator: true,
+        },
     });
     quiz = JSON.stringify(quiz);
     return {
-      props: {quiz},
+        props: { quiz },
     };
-  };
+};
 
-export default function Quiz({quiz}) {
-    quiz = JSON.parse(quiz)
-    console.log(quiz)
+export default function Quiz({ quiz }) {
+    quiz = JSON.parse(quiz);
+    const [chosenOptions, setChosenOptions] = useState(
+        Array.from({ length: quiz.questions.length }).fill(null)
+    );
+    const [results, setResults] = useState(null);
+    const [pressedSubmit, setPressedSubmit] = useState(false);
+    const [quizTaken, setQuizTaken] = useState(false);
+    const [score, setScore] = useState(0);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setPressedSubmit(true);
+        const res = await fetch("/api/results", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                chosenOptions,
+            }),
+        });
+        const results = await res.json();
+
+        if (res.ok) {
+            setResults(results.results);
+            setQuizTaken(true);
+
+            let score = 0;
+            for (let result of results.results) {
+                if (result.isCorrect) {
+                    score++;
+                }
+            }
+            setScore(score);
+        } else {
+            console.log(res.statusText);
+            setPressedSubmit(false);
+        }
+    };
     return (
         <>
-            <h1>Quiz</h1>
-            <div>
-                {quiz.title} - {quiz.creator.name}
+            <div className={styles.quizHeading}>
+                <div className={styles.quizHeadingEl}>
+                    <h3>{quiz.title}</h3>
+                </div>
+                <h3 className={styles.quizHeadingEl}>
+                    Score:{" "}
+                    {quizTaken && (
+                        <>
+                            {score}/{results.length}
+                        </>
+                    )}
+                </h3>
             </div>
+            <div>- {quiz.creator.name}</div>
+            <QuestionsList
+                questions={quiz.questions}
+                chosenOptions={chosenOptions}
+                setChosenOptions={setChosenOptions}
+                handleSubmit={handleSubmit}
+                quizTaken={quizTaken}
+                pressedSubmit={pressedSubmit}
+            />
         </>
-    )
+    );
 }
