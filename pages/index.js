3272bdaed6fa3bc5e1f5975c2aa2afeab9d2bd54
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-07 15:11:33 -0500

    updated home page, includes links to other parts of the app

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
 import styles from "../styles/Home.module.css";
-
+import Link from "next/link";
+import { useSession } from "next-auth/react";
 export default function Home() {
+    const { data: session, status } = useSession();
+
     return (
         <>
             <div className={styles.container}>
-                <h1>Welcome to Quizie</h1>
+                {status !== "loading" && (
+                    <>
+                        <h1>Welcome to Quizie</h1>
+                        <Link href="/quizies">
+                            <h3>Take a Quizie</h3>
+                        </Link>
+                        {session ? (
+                            <Link href="/create">
+                                <h3>Create a Quizie</h3>
+                            </Link>
+                        ) : (
+                            <Link href="/api/auth/signin">
+                                <h3>Login</h3>
+                            </Link>
+                        )}
+                    </>
+                )}
             </div>
         </>
     );
