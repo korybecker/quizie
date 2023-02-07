d2b5f312f2dbc4f55f22781feeb6e6f29dd3d6a2
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-07 15:15:37 -0500

    updates to theme

--- config/theme.js
+++ config/theme.js
@@ -4,14 +4,14 @@ import { red } from "@mui/material/colors";
 // create theme instance
 const theme = createTheme({
     primary: {
-        main: "#556cd6"
+        main: "#556cd6",
     },
     secondary: {
-        main: '#19857b',
+        main: "#19857b",
     },
     error: {
         main: red.A400,
-    }
-})
+    },
+});
 
 export default theme;
